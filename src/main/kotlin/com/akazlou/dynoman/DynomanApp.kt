d107bd0998676a00a6305fb04195021996f39226
Aliaksandr Kazlou <aliaksandr.kazlou@gmail.com>
2018-05-15 22:19:51 -0700

    #8 Add very basic and not very useful first view
    More like to check that everything works.

---
+++ src/main/kotlin/com/akazlou/dynoman/DynomanApp.kt
@@ -0,0 +1,5 @@
+package com.akazlou.dynoman
+
+import tornadofx.App
+
+class DynomanApp: App(MainView::class)
---
+++ src/main/kotlin/com/akazlou/dynoman/MainView.kt
@@ -0,0 +1,9 @@
+package com.akazlou.dynoman
+
+import tornadofx.View
+import tornadofx.borderpane
+
+class MainView : View("My View") {
+    override val root = borderpane {
+    }
+}
