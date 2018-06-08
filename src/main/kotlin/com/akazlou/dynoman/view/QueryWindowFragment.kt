2d1ee7dc037ef5f08d180485229242c3e481a9df
Aliaksandr Kazlou <aliaksandr.kazlou@gmail.com>
2018-06-08 21:03:31 +0300

    #25 Add index query types into the combo box
    Additionally the corresponding rows are added for the each of the indexes, although not changed currently on the combo box change.

--- src/main/kotlin/com/akazlou/dynoman/view/QueryWindowFragment.kt
+++ src/main/kotlin/com/akazlou/dynoman/view/QueryWindowFragment.kt
@@ -1,28 +1,80 @@
 package com.akazlou.dynoman.view
 
+import com.amazonaws.services.dynamodbv2.model.KeySchemaElement
+import com.amazonaws.services.dynamodbv2.model.KeyType
 import com.amazonaws.services.dynamodbv2.model.TableDescription
+import javafx.beans.property.SimpleBooleanProperty
+import javafx.geometry.Pos
+import javafx.scene.control.ComboBox
 import tornadofx.*
+import java.util.stream.Stream
 
 class QueryWindowFragment : Fragment("Query...") {
+    // TODO: make it static final const
+    private val SORT_KEY_AVAILABLE_OPERATORS: List<String> = listOf("=", "!=", ">", "<", ">=", "<=", "between")
     val description: TableDescription by param()
-    private val tableAttributesString: String
+    private val queryTypes: List<String>
+    private var queryTypeComboBox: ComboBox<String> by singleAssign()
+    private val rowObservables: List<SimpleBooleanProperty>
 
     init {
-        tableAttributesString = description.keySchema.joinToString { it.attributeName }
+
+        val tableQueryString = "[Table] ${description.tableName}: ${joinKeySchema(description.keySchema)}"
+        val indexQueryStrings = description.globalSecondaryIndexes.map { index ->
+            "[Index] ${index.indexName}: ${joinKeySchema(index.keySchema)}"
+        }
+        queryTypes = listOf(tableQueryString, *indexQueryStrings.toTypedArray())
+        rowObservables = generateSequence() {
+            SimpleBooleanProperty(false)
+        }.take(queryTypes.size).toList()
+        rowObservables[0].value = true
+    }
+
+    private fun joinKeySchema(keySchema: List<KeySchemaElement>): String {
+        return keySchema.joinToString { it.attributeName }
     }
 
     override val root = vbox {
         gridpane {
             row {
                 label("Query")
-                combobox(values = listOf("[Table] ${description.tableName}: $tableAttributesString")) {
+                queryTypeComboBox = combobox(values = queryTypes) {
                     selectionModel.select(0)
+
+                    gridpaneConstraints {
+                        columnSpan = 3
+                    }
+                }
+            }
+            val keySchemas = listOf(
+                    description.keySchema,
+                    *description.globalSecondaryIndexes.map { it.keySchema }.toTypedArray())
+            keySchemas.forEachIndexed { index, keySchema ->
+                keySchema.forEach {
+                    row {
+                        val isHash = it.keyType == KeyType.HASH.name
+                        label(if (isHash) "Partition Key" else "Sort Key")
+                        text(it.attributeName)
+                        if (isHash) {
+                            label("=")
+                        } else {
+                            combobox(values = SORT_KEY_AVAILABLE_OPERATORS)
+                        }
+                        textfield { }
+                        visibleWhen(rowObservables[index])
+                    }
                 }
             }
         }
         hbox {
-            button("Query")
+            padding = insets(5)
+            alignment = Pos.CENTER
+            button("Query") {
+                // TODO: Extract the sizes into the constant, so allow ease of modification just in one place
+                setPrefSize(100.0, 40.0)
+            }
             button("Cancel") {
+                setPrefSize(100.0, 40.0)
                 action {
                     close()
                 }
