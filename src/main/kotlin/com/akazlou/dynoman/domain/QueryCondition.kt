cad896aec954061bb6b82395fb53e98fe25e7e5e
Aliaksandr Kazlou <aliaksandr.kazlou@gmail.com>
2018-06-29 23:57:19 -0700

    #25 Apply operator to the Dynamodb query filter

--- src/main/kotlin/com/akazlou/dynoman/domain/QueryCondition.kt
+++ src/main/kotlin/com/akazlou/dynoman/domain/QueryCondition.kt
@@ -1,5 +1,7 @@
 package com.akazlou.dynoman.domain
 
+import com.amazonaws.services.dynamodbv2.document.QueryFilter
+
 data class QueryCondition(val name: String, val type: Type, val operator: Operator, val value: String)
 
 enum class Type {
@@ -19,5 +21,12 @@ enum class Operator(val text: String) {
     NOT_EXISTS("Not exists"),
     CONTAINS("Contains"),
     NOT_CONTAINS("Not Contains"),
-    BEGINS_WITH("Begins with")
+    BEGINS_WITH("Begins with");
+
+    fun <T> apply(attribute: String, type: Type, vararg values: T): QueryFilter {
+        return when (this) {
+            EQ -> QueryFilter(attribute).eq(values[0])
+            else -> throw IllegalArgumentException("Unsupported operator $this")
+        }
+    }
 }
