7ed429cd25943b2d9b0a29fce5762653be5d3f40
Aliaksandr Kazlou <aliaksandr.kazlou@gmail.com>
2018-07-06 16:12:46 -0500

    #52 Start implementation of the general parse function text function

--- src/main/kotlin/com/akazlou/dynoman/function/Function.kt
+++ src/main/kotlin/com/akazlou/dynoman/function/Function.kt
@@ -1,6 +1,38 @@
 package com.akazlou.dynoman.function
 
-interface Function<T> {
-    fun name(): String
-    fun parse(text: String): T
+abstract class Function<T> {
+    fun parse(text: String): T {
+        return run(*parseArgs(text))
+    }
+
+    private fun parseArgs(text: String): Array<String> {
+        val symbols = text.trim().drop(name().length).trim('(', ')')
+        val tokens = mutableListOf<Char>()
+        val args = mutableListOf<String>()
+        var arg = ""
+        symbols.forEach { ch ->
+            val token = if (tokens.isEmpty()) null else tokens[tokens.size - 1]
+            when (ch) {
+                '\'', '"' -> {
+                    if (ch == token) {
+                        arg += ch
+                    }
+                }
+                '(' -> {
+
+                }
+                ')' -> {
+
+                }
+                ',' -> {
+
+                }
+                else -> arg += ch
+            }
+        }
+        return emptyArray()
+    }
+
+    abstract fun name(): String
+    protected abstract fun run(vararg args: String): T
 }
