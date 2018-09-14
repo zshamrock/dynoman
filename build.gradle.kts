9d23e2535ee470dd386624cd57b24ed7665989ca
Aliaksandr Kazlou <aliaksandr.kazlou@gmail.com>
2018-09-14 10:19:43 +0300

    XXX Update Kotlin version used

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,7 @@ import org.gradle.api.tasks.testing.Test
 
 buildscript {
     var kotlin_version: String by extra
-    kotlin_version = "1.2.60"
+    kotlin_version = "1.2.61"
 
     repositories {
         mavenCentral()
