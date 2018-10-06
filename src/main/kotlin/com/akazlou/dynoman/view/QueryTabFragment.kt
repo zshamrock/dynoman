cc145b0b1e10aa172b4f8a445e11dc4eb7050479
Aliaksandr Kazlou <aliaksandr.kazlou@gmail.com>
2018-10-05 22:14:49 -0700

    #41 Prepare the UI for the data pagination

--- src/main/kotlin/com/akazlou/dynoman/view/QueryTabFragment.kt
+++ src/main/kotlin/com/akazlou/dynoman/view/QueryTabFragment.kt
@@ -8,6 +8,8 @@ import com.amazonaws.services.dynamodbv2.model.KeySchemaElement
 import javafx.beans.property.SimpleStringProperty
 import javafx.beans.value.ObservableValue
 import javafx.collections.FXCollections
+import javafx.geometry.Pos
+import javafx.scene.Cursor
 import javafx.scene.control.SelectionMode
 import javafx.scene.control.TabPane
 import javafx.scene.control.TableColumn
@@ -22,94 +24,116 @@ class QueryTabFragment : Fragment("Query Tab") {
     val operationType: OperationType by param()
     private var queryArea: TextArea by singleAssign()
     private var resultTable: TableView<ResultData> by singleAssign()
-    override val root = squeezebox {
-        fold("Query", expanded = true) {
-            tabpane {
-                tabClosingPolicy = TabPane.TabClosingPolicy.UNAVAILABLE
-                tabMinWidth = 60.0
-                val sql = tab("SQL") {
-                    queryArea = textarea("SELECT * FROM T") {
-                        vboxConstraints {
-                            prefHeight = 300.0
-                        }
-                        selectAll()
+    override val root = vbox {
+        hbox(alignment = Pos.CENTER_RIGHT) {
+            prefHeight = 150.0
+            textflow {
+                spacing = 10.0
+                text("<") {
+                    setOnMouseClicked {
+                        println("Clicked <")
                     }
+                    cursor = Cursor.HAND
                 }
-                val qwf = find<QueryWindowFragment>(
-                        params = mapOf(
-                                QueryWindowFragment::mode to QueryWindowFragment.Mode.INLINE,
-                                QueryWindowFragment::operationType to operationType,
-                                QueryWindowFragment::description to params["description"],
-                                QueryWindowFragment::operation to params["operation"]))
-                val editor = tab("EDITOR", qwf.root)
-                val queryFilters = params["queryFilters"]
-                qwf.init(operationType,
-                        params["queryType"] as QueryType?,
-                        params["hashKey"] as String?,
-                        params["sortKeyOperation"] as Operator?,
-                        (params["sortKeyValues"] as? List<*>)?.filterIsInstance<String>() ?: emptyList(),
-                        params["sort"] as String?,
-                        (queryFilters as? List<*>)?.filterIsInstance<QueryFilter>() ?: emptyList(),
-                        this@QueryTabFragment)
-                editor.select()
+                text("Viewing 101 to 200 items") {
+                }
+                text(">") {
+                    setOnMouseClicked {
+                        println("Clicked >")
+                    }
+                    cursor = Cursor.HAND
+                }
             }
         }
-        fold("Data", expanded = true) {
-            resultTable = tableview {
-                vboxConstraints {
-                    prefHeight = 335.0
-                    vGrow = Priority.ALWAYS
-                }
-                contextmenu {
-                    item("Open Value in Viewer") {
-                        setOnAction {
-                            println("Open Value in Viewer")
-                            println(selectedCell)
-                        }
-                    }
-                    separator()
-                    item("Copy Row") {
-                        setOnAction {
-                            if (selectedItem != null) {
-                                val content = (selectedItem as ResultData).getValues().joinToString {
-                                    "'$it'"
-                                }
-                                clipboard.putString(content)
+        squeezebox {
+            fold("Query", expanded = true) {
+                tabpane {
+                    tabClosingPolicy = TabPane.TabClosingPolicy.UNAVAILABLE
+                    tabMinWidth = 60.0
+                    val sql = tab("SQL") {
+                        queryArea = textarea("SELECT * FROM T") {
+                            vboxConstraints {
+                                prefHeight = 300.0
                             }
-                            println("Copy Row")
+                            selectAll()
                         }
                     }
-                    item("Copy Row (with names)") {
-                        setOnAction {
-                            if (selectedItem != null) {
-                                val resultData = selectedItem as ResultData
-                                val content = (resultData.getKeys().joinToString { it }
-                                        + "\n"
-                                        + resultData.getValues().joinToString {
-                                    "'$it'"
-                                })
-                                clipboard.putString(content)
-                            }
-                            println("Copy Row (with names)")
-                        }
-                    }
-                    item("Copy Field", KeyCombination.keyCombination("Ctrl+C")) {
-                        setOnAction {
-                            println("Copy Field")
-                            if (selectedValue != null) {
-                                clipboard.putString(selectedValue as String)
-                            }
-                            println(selectedCell)
-                            println(selectedColumn)
-                            println(selectedItem)
-                            println(selectedValue)
-                        }
-                    }
+                    val qwf = find<QueryWindowFragment>(
+                            params = mapOf(
+                                    QueryWindowFragment::mode to QueryWindowFragment.Mode.INLINE,
+                                    QueryWindowFragment::operationType to operationType,
+                                    QueryWindowFragment::description to params["description"],
+                                    QueryWindowFragment::operation to params["operation"]))
+                    val editor = tab("EDITOR", qwf.root)
+                    val queryFilters = params["queryFilters"]
+                    qwf.init(operationType,
+                            params["queryType"] as QueryType?,
+                            params["hashKey"] as String?,
+                            params["sortKeyOperation"] as Operator?,
+                            (params["sortKeyValues"] as? List<*>)?.filterIsInstance<String>() ?: emptyList(),
+                            params["sort"] as String?,
+                            (queryFilters as? List<*>)?.filterIsInstance<QueryFilter>() ?: emptyList(),
+                            this@QueryTabFragment)
+                    editor.select()
                 }
             }
-            with(resultTable.selectionModel) {
-                isCellSelectionEnabled = true
-                selectionMode = SelectionMode.SINGLE
+            fold("Data", expanded = true) {
+                resultTable = tableview {
+                    vboxConstraints {
+                        prefHeight = 335.0
+                        vGrow = Priority.ALWAYS
+                    }
+                    contextmenu {
+                        item("Open Value in Viewer") {
+                            setOnAction {
+                                println("Open Value in Viewer")
+                                println(selectedCell)
+                            }
+                        }
+                        separator()
+                        item("Copy Row") {
+                            setOnAction {
+                                if (selectedItem != null) {
+                                    val content = (selectedItem as ResultData).getValues().joinToString {
+                                        "'$it'"
+                                    }
+                                    clipboard.putString(content)
+                                }
+                                println("Copy Row")
+                            }
+                        }
+                        item("Copy Row (with names)") {
+                            setOnAction {
+                                if (selectedItem != null) {
+                                    val resultData = selectedItem as ResultData
+                                    val content = (resultData.getKeys().joinToString { it }
+                                            + "\n"
+                                            + resultData.getValues().joinToString {
+                                        "'$it'"
+                                    })
+                                    clipboard.putString(content)
+                                }
+                                println("Copy Row (with names)")
+                            }
+                        }
+                        item("Copy Field", KeyCombination.keyCombination("Ctrl+C")) {
+                            setOnAction {
+                                println("Copy Field")
+                                if (selectedValue != null) {
+                                    clipboard.putString(selectedValue as String)
+                                }
+                                println(selectedCell)
+                                println(selectedColumn)
+                                println(selectedItem)
+                                println(selectedValue)
+                            }
+                        }
+                    }
+                }
+                with(resultTable.selectionModel) {
+                    isCellSelectionEnabled = true
+                    selectionMode = SelectionMode.SINGLE
+                }
             }
         }
     }
