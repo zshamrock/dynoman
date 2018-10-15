15d2f613028f1af4fd174477206bd628298e67fe
Aliaksandr Kazlou <aliaksandr.kazlou@gmail.com>
2018-10-14 23:29:19 -0700

    #4 Test query spec construction from query search

--- src/test/kotlin/com/akazlou/dynoman/domain/QuerySearchSpec.kt
+++ src/test/kotlin/com/akazlou/dynoman/domain/QuerySearchSpec.kt
@@ -1,14 +1,25 @@
 package com.akazlou.dynoman.domain
 
+import com.amazonaws.services.dynamodbv2.document.KeyAttribute
+import io.kotlintest.matchers.beEmpty
+import io.kotlintest.matchers.boolean.shouldBeFalse
+import io.kotlintest.should
+import io.kotlintest.shouldBe
 import io.kotlintest.specs.StringSpec
 
-class QuerySearchSpec: StringSpec({
+class QuerySearchSpec : StringSpec({
     "build query spec" {
         val search = QuerySearch(
                 "TableA",
                 null,
-                listOf(QueryCondition("Id", Type.NUMBER, Operator.EQ, listOf(""))),
+                listOf(QueryCondition("Id", Type.NUMBER, Operator.EQ, listOf("1"))),
                 listOf(),
                 Order.DESC)
+        val spec = search.toQuerySpec(0)
+        spec.hashKey shouldBe KeyAttribute("Id", 1L)
+        spec.rangeKeyCondition shouldBe null
+        spec.queryFilters should beEmpty()
+        spec.isScanIndexForward.shouldBeFalse()
+        spec.maxPageSize shouldBe null
     }
 })
