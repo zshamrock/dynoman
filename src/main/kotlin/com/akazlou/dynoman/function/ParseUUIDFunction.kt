91b4028d55107434ee4f0a81149f73603e73486d
Aliaksandr Kazlou <aliaksandr.kazlou@gmail.com>
2018-11-08 23:50:36 -0800

    XXX Hack commit

--- src/main/kotlin/com/akazlou/dynoman/function/ParseUUIDFunction.kt
+++ src/main/kotlin/com/akazlou/dynoman/function/ParseUUIDFunction.kt
@@ -4,6 +4,8 @@ import java.util.Locale
 import java.util.UUID
 
 class ParseUUIDFunction : Function<String>() {
+    val SEPARATOR: String = "-"
+
     override fun name(): String {
         return "parse_uuid"
     }
