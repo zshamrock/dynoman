6daecc0b8949f39468ebfe63707c5a333196ed97
Aliaksandr Kazlou <aliaksandr.kazlou@gmail.com>
2019-03-11 11:51:52 +0300

    #4 Research on getting the correct path to store the search criteria

--- src/main/kotlin/com/akazlou/dynoman/view/SaveQueryFragment.kt
+++ src/main/kotlin/com/akazlou/dynoman/view/SaveQueryFragment.kt
@@ -2,26 +2,34 @@ package com.akazlou.dynoman.view
 
 import com.akazlou.dynoman.controller.SessionSaverController
 import com.akazlou.dynoman.domain.search.SearchCriteria
+import javafx.beans.property.SimpleStringProperty
 import javafx.geometry.Pos
 import tornadofx.*
 
 // TODO: Define the structure/format of the data stored, and where
 class SaveQueryFragment : Fragment("Save Query") {
+
     val criterias: List<SearchCriteria> by param()
     val controller: SessionSaverController by inject()
+
+    private val sessionNameProperty = SimpleStringProperty()
 
     // TODO: Use TornadoFX form layout to organize the fields
     override val root = vbox(5.0) {
         hbox(5.0) {
             label("Name: ")
-            textfield()
+            textfield(sessionNameProperty)
             alignment = Pos.CENTER
         }
         hbox(5.0) {
             button("Save") {
                 action {
+                    val path = app.configBasePath
+                            .resolve("session")
+                            .resolve("${sessionNameProperty.value}.session")
                     controller.save(criterias, config)
                     println("Saving $criterias")
+                    //Files.write(path, )
                     close()
                 }
             }
