80ef4c293101dce9306259c74cb739c1769f9430
Aliaksandr Kazlou <aliaksandr.kazlou@gmail.com>
2019-03-27 17:39:44 +0300

    #4 Use form layout instead of box-es

--- src/main/kotlin/com/akazlou/dynoman/view/SaveQueryFragment.kt
+++ src/main/kotlin/com/akazlou/dynoman/view/SaveQueryFragment.kt
@@ -3,7 +3,6 @@ package com.akazlou.dynoman.view
 import com.akazlou.dynoman.controller.SessionSaverController
 import com.akazlou.dynoman.domain.search.SearchCriteria
 import javafx.beans.property.SimpleStringProperty
-import javafx.geometry.Pos
 import tornadofx.*
 
 // TODO: Define the structure/format of the data stored, and where
@@ -14,31 +13,29 @@ class SaveQueryFragment : Fragment("Save Query") {
 
     private val sessionNameProperty = SimpleStringProperty()
 
-    // TODO: Use TornadoFX form layout to organize the fields
-    override val root = vbox(5.0) {
-        hbox(5.0) {
-            label("Name: ")
-            textfield(sessionNameProperty)
-            alignment = Pos.CENTER
-        }
-        hbox(5.0) {
-            button("Save") {
-                action {
-                    val path = app.configBasePath
-                            .resolve("session")
-                            .resolve("${sessionNameProperty.value}.session")
-                    controller.save(path, criterias, config)
-                    println("Saving $criterias at $path")
-                    //Files.write(path, )
-                    close()
+    override val root = form {
+        fieldset {
+            field("Name") {
+                textfield(sessionNameProperty)
+            }
+            buttonbar {
+                button("Save") {
+                    action {
+                        val path = app.configBasePath
+                                .resolve("session")
+                                .resolve("${sessionNameProperty.value}.session")
+                        controller.save(path, criterias, config)
+                        println("Saving $criterias at $path")
+                        //Files.write(path, )
+                        close()
+                    }
+                }
+                button("Cancel") {
+                    action {
+                        close()
+                    }
                 }
             }
-            button("Cancel") {
-                action {
-                    close()
-                }
-            }
-            alignment = Pos.CENTER
         }
     }
 }
