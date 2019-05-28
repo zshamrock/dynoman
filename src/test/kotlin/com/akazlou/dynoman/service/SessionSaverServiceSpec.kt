93cedc7a4d4acd9bc38d4132dff87cc234788770
Aliaksandr Kazlou <aliaksandr.kazlou@gmail.com>
2019-05-28 09:33:32 +0300

    #4 Setup test class for verify sessions JSON serialization and deserialization

--- src/test/kotlin/com/akazlou/dynoman/service/SessionSaverServiceSpec.kt
+++ src/test/kotlin/com/akazlou/dynoman/service/SessionSaverServiceSpec.kt
@@ -1,9 +1,31 @@
 package com.akazlou.dynoman.service
 
+import io.kotlintest.Matcher
+import io.kotlintest.Result
+import io.kotlintest.shouldBe
 import io.kotlintest.specs.StringSpec
+import java.nio.file.Paths
 
-class SessionSaverServiceSpec: StringSpec({
-    "save sessions" {
+class SessionSaverServiceSpec : StringSpec() {
+    private val base = Paths.get("src", "test", "resources", "sessions")
 
+    init {
+        "save empty sessions" {
+            val service = SessionSaverService()
+            service.save(base, "test1_actual", listOf())
+            "test1" shouldBe sameContent()
+        }
     }
-})
+
+    private fun sameContent() = object : Matcher<String> {
+        override fun test(value: String): Result {
+            val actual = "${value}_actual.session"
+            val expected = "${value}_expected.session"
+            return Result(
+                    base.resolve(actual).toFile().readText() == base.resolve(expected).toFile().readText(),
+                    "Contents of the files $actual and $expected don't match",
+                    "Contents of the files $actual and $expected match"
+            )
+        }
+    }
+}
