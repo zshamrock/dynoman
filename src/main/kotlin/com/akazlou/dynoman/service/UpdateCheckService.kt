68bde0108b86dd37180f7b6c204fe076fc7560c7
Aliaksandr Kazlou <aliaksandr.kazlou@gmail.com>
2019-09-21 18:52:33 +0300

    #230 Refactor extract separate method for getting system property as boolean

--- src/main/kotlin/com/akazlou/dynoman/service/UpdateCheckService.kt
+++ src/main/kotlin/com/akazlou/dynoman/service/UpdateCheckService.kt
@@ -10,12 +10,15 @@ import java.nio.file.Paths
 class UpdateCheckService {
     companion object {
         private const val BASE_RELEASE_URL = "https://raw.githubusercontent.com/zshamrock/dynoman/master/release"
+        private const val PROPERTY_PREFIX = "dynoman"
+        private const val RELEASE_DIR = "release"
+
         @JvmField
         val DEFAULT_ANNOUNCEMENT = UpdateAnnouncement(version = Version.CURRENT)
     }
 
-    private val debug = (System.getProperty("dynoman.debug").ifEmpty { "true" } ?: "false").toBoolean()
-    private val noUpdate = (System.getProperty("dynoman.noupdate") ?: "false").toBoolean()
+    private val debug = getSystemBooleanProperty("$PROPERTY_PREFIX.debug", true)
+    private val noUpdate = getSystemBooleanProperty("$PROPERTY_PREFIX.noupdate")
 
     private enum class Type(val filename: String) {
         VERSION("version"),
@@ -28,7 +31,6 @@ class UpdateCheckService {
 
     fun getUpdate(): UpdateAnnouncement {
         if (noUpdate) {
-            println("Skip checking update on start up")
             return DEFAULT_ANNOUNCEMENT
         }
         var version: String = Version.CURRENT
@@ -51,7 +53,7 @@ class UpdateCheckService {
 
     private fun get(type: Type, def: String = ""): String {
         if (debug) {
-            return Files.readAllLines(Paths.get(System.getProperty("user.dir"), "release", type.filename))
+            return Files.readAllLines(Paths.get(System.getProperty("user.dir"), RELEASE_DIR, type.filename))
                     .joinToString(separator = System.lineSeparator())
         }
         val call = client.newCall(Request.Builder()
@@ -60,5 +62,15 @@ class UpdateCheckService {
         return call.execute().use { response ->
             response.body?.string() ?: def
         }
+    }
+
+    private fun getSystemBooleanProperty(name: String, presenceAsTrue: Boolean = false): Boolean {
+        return System.getProperty(name).orEmpty().ifEmpty {
+            if (presenceAsTrue) {
+                "true"
+            } else {
+                "false"
+            }
+        }.toBoolean()
     }
 }
