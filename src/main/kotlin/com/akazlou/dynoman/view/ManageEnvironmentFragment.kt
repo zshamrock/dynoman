70e50295816afbcbafde582b18ee55e3701c5d42
Aliaksandr Kazlou <aliaksandr.kazlou@gmail.com>
2019-12-01 23:47:28 +0300

    #5 Add buttons to control table items

--- src/main/kotlin/com/akazlou/dynoman/view/ManageEnvironmentFragment.kt
+++ src/main/kotlin/com/akazlou/dynoman/view/ManageEnvironmentFragment.kt
@@ -4,15 +4,28 @@ import com.akazlou.dynoman.controller.ManagedEnvironmentsController
 import com.akazlou.dynoman.domain.EnvironmentValue
 import javafx.geometry.Pos
 import tornadofx.*
+import java.util.UUID
 
 class ManageEnvironmentFragment : Fragment("Manage Environments") {
     private val controller: ManagedEnvironmentsController by inject()
+    private val items = controller.getGlobals().values.toMutableList().asObservable()
 
     override val root = vbox {
         prefWidth = 810.0
         prefHeight = 430.0
         alignment = Pos.CENTER
-        tableview(controller.getGlobals().values.asObservable()) {
+        buttonbar {
+            button("+") {
+                addClass("button-x")
+                action {
+                    items.add(EnvironmentValue(UUID.randomUUID().toString(), UUID.randomUUID().toString()))
+                }
+            }
+            button("-") {
+                addClass("button-x")
+            }
+        }
+        tableview(items) {
             isEditable = true
             column("Variable", EnvironmentValue::nameProperty).makeEditable()
             column("Value", EnvironmentValue::valueProperty).makeEditable()
