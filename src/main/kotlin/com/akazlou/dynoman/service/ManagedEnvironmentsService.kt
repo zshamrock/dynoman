181e174ae4134ff402ef2da668107c344eda6c8b
Aliaksandr Kazlou <aliaksandr.kazlou@gmail.com>
2020-02-06 00:53:38 +0300

    #5 Restore env vars from file

--- src/main/kotlin/com/akazlou/dynoman/service/ManagedEnvironmentsService.kt
+++ src/main/kotlin/com/akazlou/dynoman/service/ManagedEnvironmentsService.kt
@@ -1,7 +1,10 @@
 package com.akazlou.dynoman.service
 
+import com.akazlou.dynoman.domain.EnvironmentValue
 import com.akazlou.dynoman.domain.ManagedEnvironment
 import java.io.StringWriter
+import java.nio.charset.StandardCharsets
+import java.nio.file.Files
 import java.nio.file.Path
 
 class ManagedEnvironmentsService : SaverService() {
@@ -11,5 +14,11 @@ class ManagedEnvironmentsService : SaverService() {
             writer.appendln(it.name + "=" + it.value)
         }
         write(base, environment.name, ".env", writer)
+    }
+
+    fun restore(base: Path, name: String): ManagedEnvironment {
+        val path = resolve(base, name, ".env")
+        val lines = Files.readAllLines(path, StandardCharsets.UTF_8)
+        return ManagedEnvironment("Globals", lines.filter { it.isNotBlank() }.map { it.split("=") }.map { EnvironmentValue(it[0], it[1]) })
     }
 }
